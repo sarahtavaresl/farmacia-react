f7a3c85fbf1ee592c98efb9824dbaeae49fe3eb6
Sarah Tavares de Lima <sarahtavarez.lima@gmail.com>
2025-01-27 18:01:02 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import "./App.css";
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
-        <Home />
+        <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,8 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
+            <div className="bg-indigo-400 flex justify-center">
+                <div className='container grid grid-cols-2 text-red'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Seja Bem Vinde!
